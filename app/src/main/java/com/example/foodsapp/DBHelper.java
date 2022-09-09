baef81bb62abf680b2dbac08887247e02cb48b83
Damian Stopa <stopa.damian997@gmail.com>
2022-09-09 16:21:02 +0200

    First part View of Fridge

--- app/src/main/java/com/example/foodsapp/DBHelper.java
+++ app/src/main/java/com/example/foodsapp/DBHelper.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.SimpleCursorAdapter;
+import android.widget.SimpleExpandableListAdapter;
 
 import androidx.annotation.Nullable;
 
@@ -12,9 +14,11 @@ public class DBHelper extends SQLiteOpenHelper {
 
 
     public static final String DBNAME = "Login.db";
+    Context context;
 
     public DBHelper(@Nullable Context context) {
         super(context, "Login.db", null, 1);
+        this.context = context;
     }
 
     @Override
@@ -56,4 +60,19 @@ public class DBHelper extends SQLiteOpenHelper {
         else
             return false;
     }
+
+    public SimpleCursorAdapter populateListViewFromDB(){
+        String columns[]={DBHelper.KEY_ROWID,DBHelper.KEY_NAME, DBHelper.KEY_EMAIL};
+        SQLiteDatabase MyDB = this.getWritableDatabase();
+        Cursor cursor = MyDB.query(DBHelper.TABLE_NAME,columns, null,null,null,null,null);
+        String[] fromFieldNames = new String[]{
+                DBHelper.KEY_ROWID,DBHelper.KEY_NAME, DBHelper.KEY_EMAIL
+        };
+
+        int [] toViewIDs = new int [] {R.id.item_id, R.id.item_name, R.id.item_email};
+        SimpleCursorAdapter fridgeAdapter = new SimpleCursorAdapter(
+            context, R.layout.single_item, cursor, fromFieldNames, toViewIDs
+        );
+        return fridgeAdapter;
+    };
 }
